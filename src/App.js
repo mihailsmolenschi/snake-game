3250c24c374a340c8ab14849162ba24ff31ebb59
ctrlgear <smolenschi.mihai@gmail.com>
2022-06-28 23:59:19 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,197 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { useEffect, useState } from "react";
+import { GRID_SIZE } from "./config";
 
-function App() {
+const inintialSnakePosition = {
+  gridRow: 1,
+  gridColumn: 1,
+};
+
+const generateRandomFoodPosition = () => {
+  return {
+    gridRow: Math.ceil(Math.random() * GRID_SIZE.gridRow),
+    gridColumn: Math.ceil(Math.random() * GRID_SIZE.gridColumn),
+  };
+};
+
+const App = () => {
+  const [snakePosition, setSnakePosition] = useState({
+    ...inintialSnakePosition,
+  });
+  const [foodPosition, setFoodPosition] = useState(
+    generateRandomFoodPosition()
+  );
+  const [score, setScore] = useState(0);
+  const [direction, setDirection] = useState("");
+  const [snake, setSnake] = useState({
+    tail: 0,
+  });
+  const [speed, setSpeed] = useState(200);
+
+  console.log(GRID_SIZE.gridColumn);
+
+  const onClickHandler = (event) => {
+    const { name } = event.target;
+
+    setDirection(name);
+
+    checkPositions();
+  };
+
+  const checkPositions = () => {
+    if (
+      snakePosition.gridColumn === foodPosition.gridColumn &&
+      snakePosition.gridRow === foodPosition.gridRow
+    ) {
+      setScore(score + 1);
+      setSnake({ ...snake, tail: snake.tail + 1 });
+      setFoodPosition(generateRandomFoodPosition);
+    }
+    if (
+      snakePosition.gridColumn > GRID_SIZE.gridColumn ||
+      snakePosition.gridColumn < 1 ||
+      snakePosition.gridRow > GRID_SIZE.gridRow ||
+      snakePosition.gridRow < 1 ||
+      direction === "escape"
+    ) {
+      console.log("hello bleaghi");
+      setSnakePosition(inintialSnakePosition);
+      setDirection("");
+      setScore(0);
+      setFoodPosition(generateRandomFoodPosition);
+    }
+  };
+  useEffect(checkPositions);
+
+  const reportPosition = () => console.log(snakePosition, foodPosition);
+
+  // -----------------------------------
+  // Move
+  // -----------------------------------
+  const move = (direction) => {
+    if (direction === "up" || direction === "w" || direction === "arrowup") {
+      setSnakePosition({
+        ...snakePosition,
+        gridRow: snakePosition.gridRow - 1,
+      });
+    }
+    if (
+      direction === "down" ||
+      direction === "s" ||
+      direction === "arrowdown"
+    ) {
+      setSnakePosition({
+        ...snakePosition,
+        gridRow: snakePosition.gridRow + 1,
+      });
+    }
+    if (
+      direction === "left" ||
+      direction === "a" ||
+      direction === "arrowleft"
+    ) {
+      setSnakePosition({
+        ...snakePosition,
+        gridColumn: snakePosition.gridColumn - 1,
+      });
+    }
+    if (
+      direction === "right" ||
+      direction === "d" ||
+      direction === "arrowright"
+    ) {
+      setSnakePosition({
+        ...snakePosition,
+        gridColumn: snakePosition.gridColumn + 1,
+      });
+    }
+    console.log(direction);
+  };
+
+  useEffect(() => {
+    // on key down
+    document.addEventListener("keydown", (event) => {
+      console.log(event.key);
+      setDirection(event.key.toLowerCase());
+    });
+
+    const interval = setInterval(() => {
+      move(direction);
+    }, speed);
+
+    return () => clearInterval(interval);
+  });
+
+  const changeSpeedHandler = (event) => {
+    const { value } = event.target;
+    console.log(value);
+
+    if (value === "+") {
+      setSpeed(speed + 10);
+    }
+    if (value === "-") {
+      setSpeed(speed - 10);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Snake</h1>
+
+      <h3>Score: {score}</h3>
+
+      <div className="grid">
+        <div
+          className="snake"
+          style={{
+            gridRow: snakePosition.gridRow,
+            gridColumn: snakePosition.gridColumn,
+          }}
+          onClick={reportPosition}
         >
-          Learn React
-        </a>
-      </header>
+          &nbsp;
+        </div>
+        <div
+          className="food"
+          style={{
+            gridRow: foodPosition.gridRow,
+            gridColumn: foodPosition.gridColumn,
+          }}
+          onClick={reportPosition}
+        >
+          &nbsp;
+        </div>
+      </div>
+
+      <div className="settings">
+        <div className="speed-container">
+          <button onClick={changeSpeedHandler} value="-">
+            -
+          </button>
+          <label>speed</label>
+          <button onClick={changeSpeedHandler} value="+">
+            +
+          </button>
+          <span>{speed}</span>
+        </div>
+      </div>
+
+      <div className="controls">
+        <button onClick={onClickHandler} name="up" className="up">
+          up
+        </button>
+        <button onClick={onClickHandler} name="down" className="down">
+          down
+        </button>
+        <button onClick={onClickHandler} name="right" className="right">
+          right
+        </button>
+        <button onClick={onClickHandler} name="left" className="left">
+          left
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const GRID_SIZE = {
+  gridRow: 20,
+  gridColumn: 20,
+};
